9ad71399c4a1555ec0a1db80ac0db1c725aba68a
Helena <nightsunHCT@users.noreply.github.com>
2020-05-10 22:46:28 -0400

    set up

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // later
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[System.Serializable]
+public class Decorator
+{
+    public string type;
+    public Vector3 loc;
+    public bool flip = false;
+    public float scale = 1f;
+}
+
+[System.Serializable]
+public class CardDefinition
+{
+    public string face;
+    public int rank;
+    public List<Decorator> pips = new List<Decorator>();
 }
