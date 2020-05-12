7d30ab97c927c903b2ccee40c3a6e3c4b9931ca9
Helena <nightsunHCT@users.noreply.github.com>
2020-05-12 15:44:39 -0400

    showing scores

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -18,6 +18,8 @@ public class Prospector : MonoBehaviour
     public Vector2 fsPosRun = new Vector2(.5f, .75f);
     public Vector2 fsPosMid2 = new Vector2(.4f, 1.0f);
     public Vector2 fsPosEnd = new Vector2(.5f, .95f);
+    public float reloadDelay = 1f;
+    public Text gameOverText, roundResultText, highScoreText;
 
     [Header("Set Dynamically")]
     public Deck deck;
@@ -32,7 +34,40 @@ public class Prospector : MonoBehaviour
     void Awake()
     {
         S = this;
+        SetUpUITexts();
 
+    }
+
+    void SetUpUITexts()
+    {
+        GameObject go = GameObject.Find("HighScore");
+        if (go != null)
+        {
+            highScoreText = go.GetComponent<Text>();
+        }
+        int highScore = ScoreManager.HIGH_SCORE;
+        string hScore = "High Score: " + Utils.AddCommasToNumber(highScore);
+        go.GetComponent<Text>().text = hScore;
+
+        go = GameObject.Find("GameOver");
+        if (go != null)
+        {
+            gameOverText = go.GetComponent<Text>();
+        }
+
+        go = GameObject.Find("RoundResult");
+        if (go != null)
+        {
+            roundResultText = go.GetComponent<Text>();
+        }
+
+        ShowResultsUI(false);
+    }
+
+    void ShowResultsUI(bool show)
+    {
+        gameOverText.gameObject.SetActive(show);
+        roundResultText.gameObject.SetActive(show);
     }
 
     void Start()
@@ -259,22 +294,43 @@ public class Prospector : MonoBehaviour
     }
 
     void GameOver (bool won)
-
     {
+        int score = ScoreManager.SCORE;
+        if (fsRun != null) score += fsRun.score;
         if (won)
         {
+            gameOverText.text = "Round Over";
+            roundResultText.text = "You won this round!\nRound Score: " + score;
+            ShowResultsUI(true);
             // print("Game Over. You won! :)");
             ScoreManager.EVENT(eScoreEvent.gameWin);
             FloatingScoreHandler(eScoreEvent.gameWin);
         }
         else
         {
+            gameOverText.text = "Game Over";
+            if (ScoreManager.HIGH_SCORE <= score)
+            {
+                string str = "You got the high score! \nHigh score: " + score;
+                roundResultText.text = str;
+            }
+            else
+            {
+                roundResultText.text = "Your final score was: " + score;
+            }
+            ShowResultsUI(true);
             //print("Game Over. You lost. :(");
             ScoreManager.EVENT(eScoreEvent.gameLoss);
             FloatingScoreHandler(eScoreEvent.gameLoss);
         }
-        SceneManager.LoadScene("__Prospector_Scene_0");
+        // SceneManager.LoadScene("__Prospector_Scene_0");
+        Invoke("ReloadLevel", reloadDelay);
         
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene("__Prospector_Scene_0");
     }
 
     public bool AdjacentRank (CardProspector c0, CardProspector c1)
